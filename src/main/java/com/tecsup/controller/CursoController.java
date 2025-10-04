e89d5dc1a5cc170a9ac22e57676ed6429e7fe5ab
fonseca <fonseca010101@gmail.com>
2025-10-04 15:31:35 -0500

    septimo intento

--- src/main/java/com/tecsup/controller/CursoController.java
+++ src/main/java/com/tecsup/controller/CursoController.java
@@ -5,6 +5,7 @@ import com.tecsup.services.CursoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
@@ -29,6 +30,24 @@ public class CursoController {
         model.addAttribute("cursos",servicio.FindAllCourse());
         return "listView";
     }
+
+    public String crear(Model model) {
+        Curso curso = new Curso();  // Crear un nuevo objeto Curso
+        model.addAttribute("curso", curso);
+        model.addAttribute("titulo", "Formulario de Curso");
+        return "formView";  // vista para el formulario
+    }
+
+    public String guardar(Curso curso, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            model.addAttribute("titulo", "Formulario de Curso");
+            return "formView";  // Regresa al formulario en caso de error
+        }
+
+        servicio.SaveCourse(curso);  // Guardar el curso
+        return "redirect:/listar";  // Redirigir a la lista de cursos
+    }
+
 
 
 
